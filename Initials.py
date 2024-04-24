7133107d485adadee3dc308b4a773dd75c42b111
Philip Eze <167911915+PhilipEze@users.noreply.github.com>
2024-04-24 03:45:56 +0200

    Create Initials.py
    Create initials of my full name using "Multi-line strings".

---
+++ Initials.py
@@ -0,0 +1,11 @@
+#Philip Chidera Eze
+#I am a perfectionist and easily irritated, yikes!
+print("""
+PPPP      CCC     EEEEE
+P   P    C   C    E
+PPPP     C        E
+P        C        EEEE
+P        C        E
+P        C   C    E
+P         CCC     EEEEE
+""")
