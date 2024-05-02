367ca62562de729361e0e049b677a2b9417ae575
Philip Eze <167911915+PhilipEze@users.noreply.github.com>
2024-05-02 10:52:53 +0200

    Update shipping.py

--- shipping.py
+++ shipping.py
@@ -22,13 +22,13 @@ print("Ground Shipping Premium cost = $"+str(premium_ground_price))
 
 #Drone shipping
 if weight <= 2:
-  ground_cost =4.50*weight
+  drone_cost =4.50*weight
 elif weight <= 6:
-  ground_cost =9.00*weight
+  drone_cost =9.00*weight
 elif weight <= 10:
-  ground_cost =12.00*weight
+  drone_cost =12.00*weight
 elif weight > 10:
-  ground_cost =14.25*weight
+  drone_cost =14.25*weight
 else:
   print("Error")
 print(50*"*")  
@@ -38,3 +38,7 @@ print("Drone Shipping cost = $"+str(round(ground_cost,2)))
 
 # Cheapest method to ship --> 4.8lb is Ground Shipping.
 # Cheapest method to ship --> 41.5lb is Ground Shipping Premium.
+
+
+
+
